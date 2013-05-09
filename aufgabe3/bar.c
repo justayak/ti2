5a7191a78bb4dc43e08b60ab2f7c52549befce72
justayak <justayak@googlemail.com>
2013-05-09 17:52:25 +0200

    stuff

--- aufgabe3/bar.c
+++ aufgabe3/bar.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-int gauss(unsigned int);
+int fibonacci(unsigned int);
 
 int main()
 {
-	int i = gauss(5);
+	int i = fibonacci(5);
 	printf("i hat den Wert: %d",i);
 	return 0;
 
