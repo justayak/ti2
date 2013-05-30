3c625b6b005914ec9a534e38108800ff66929f73
Julian <julian@julian-Spring-Peak.(none)>
2013-05-30 20:16:52 +0200

    done

--- aufgabe6/bar.c
+++ aufgabe6/bar.c
@@ -3,12 +3,13 @@
 
 // Unsere NASM-Funktion bekommt ein Zeiger auf das Array und die Anzahl der
 // Elemente in dem Array übergeben
-int bubble_sort(int64_t[], uint64_t);
+void bubble_sort(int64_t[], uint64_t);
 
-unsigned int main() {
+int main() {
     // Erstelle Array mit ein paar Zahlen
-    int64_t array[] = {6,8,2,6,-5,3,-1,9,8,0,6,4,5,-8,-4,3,2,6};
-    uint64_t num_elements = sizeof(array)/sizeof(int64_t);
+    //int64_t array[] = {6,8,2,6,-5,3,-1,9,8,0,6,4,5,-8,-4,3,2,6};
+    int64_t array[] = {2,3,4,1,5,6,9,8};
+    int num_elements = sizeof(array)/sizeof(int64_t);
     int64_t i;
 
     printf("groesse: %u\n", num_elements);
@@ -16,8 +17,8 @@ unsigned int main() {
     for (i = 0; i < num_elements; ++i) { printf("%i, ", array[i]); }
 
     // Sortieren
-    int result = bubble_sort(array,num_elements);
-    printf("\nresult: %u\n", result);
+    bubble_sort(array,num_elements);
+    //printf("\nresult: %u\n", result);
 
 
     printf("\nnachher: ");
--- aufgabe6/foo.asm
+++ aufgabe6/foo.asm
@@ -9,32 +9,50 @@ global bubble_sort
 bubble_sort:
 	; rdx = ListLength
 	
+	mov rsi, rax; save list
 	mov r12, rdx; save list length
 	mov r8, rdx ; outer index
 	mov r9, 0 ; inner index
+	mov r13, 8 ; const 8
+
+	mov rcx, 0 ; debug
 
 outerLoop:
 	cmp r8, 1
 	je end		
 	mov r10, r8
-	sub r10, 1
+	sub r10, 1	
 innerLoop:
 	cmp r9, r10
-	jl outerContinue
-	;cmp rax[r9 * 8], rax[r9 + 8 * 1]
-	jle innerContinue 	
+	jg outerContinue
 	
-	mov rdx, r9
-	mul 8
-	mov r11, rax[rdx]
-	;mov rax[r9 *8], rax[r9 + 8*1]
-	;mov rax[r9 + 8*1], r11	
+	add rcx, 1	
 
+	mov rax, r9
+	mul r13	
+	
+	mov r15, [rsi + rax] 
+	mov rbx, [rsi + rax + 8]	
+	cmp r15, rbx
+	;jle innerContinue
+	jge innerContinue 	
+
+	mov r11, [rsi + rax ] ;r11 = temp
+	;mov [rsi + rax], [rsi + rax + 8]
+	mov rbx, [rsi + rax + 8] ; hier ist der bug..
+	mov [rsi + rax], rbx
+	mov [rsi + rax + 8], r11
+	
 	
 innerContinue:
 	inc r9	
+	jmp innerLoop
 	; inner loop end
 outerContinue:
+	mov r9, 0
 	dec r8
+	jmp outerLoop
 end:
+
+	mov rax, rcx
     	ret
