8a55d286d25b1658e20c23b12b99b65898e276ec
Julian <julian@julian-VirtualBox.(none)>
2013-06-06 20:49:09 +0200

    stuff for 7

--- aufgabe7/bar.asm
+++ aufgabe7/bar.asm
@@ -1,8 +1,3 @@
-; Programm-Bereich in dem wir im vorhinein Speicher reservieren können
-section .bss
-; Reserviere 1 mal 64bit Speicher
-temp:   resq 1
-
 section .text
 
 global BCD_Convert
@@ -12,7 +7,6 @@ BCD_Convert:
 	mov rdx, 0
 	mov r9, 10;...
 	mov r10, 0 ; result
-	;move cl, 4
 	
 ; find biggest divisor_10
 fd_loop:
@@ -22,7 +16,7 @@ fd_loop:
 
 	mov rdx,0	
 	div r9
-	div r9 ; wtf warum funzt das ?
+	div r9 
 
 loop:
 	cmp rax, 1
@@ -39,22 +33,65 @@ loop:
 
 	mov rsi, rax ; save 0-9
 	mov rax, r10
-	shl rax, 4
-	
+	shl rax, 8
+
 	cmp rsi, 9
 	jl eight
-	mov al, 0x9
+	mov al, 0x48  ; 1001 0000
 	jmp continueLoop
 eight:
+	cmp rsi, 8
+	jl seven
+	mov al, 0x40 	;1000 0000
+	jmp continueLoop
+seven:
+	cmp rsi, 7
+	jl six
+	mov al, 0x70
+	jmp continueLoop
+six:
+	cmp rsi, 6
+	jl five
+	mov al, 0x60
+	jmp continueLoop
+five:
+	cmp rsi, 5
+	jl four
+	mov al, 0x50
+	jmp continueLoop
+four:
+	cmp rsi, 4
+	jl three
+	mov al, 0x40
+	jmp continueLoop
+three:
+	cmp rsi, 3
+	jl two
+	mov al, 0x30
+	jmp continueLoop
+two:
+	cmp rsi, 2
+	jl one
+	mov al, 0x20
+	jmp continueLoop
+one:
+	cmp rsi, 1
+	jl zero
+	mov al, 0x10
+	jmp continueLoop
+zero:
+	mov al, 0x0
 
 ;----calcs end!----
 continueLoop:
-
+	shr rax, 4
+	mov rdx, 0
+	mov r10, rax
 	mov rax, r11 ; restore rax
 	div r9
 	jmp loop
 
 end:
-	;mov al, 5
 	mov rax, r10
+
     	ret
